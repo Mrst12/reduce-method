adb6a52c9bebed54f77eb721fd59dd1227042ddc
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-02-25 10:09:56 +0000

    Add more examples of reduce method

--- reduce.js
+++ reduce.js
@@ -6,16 +6,16 @@
 
 
 // Summing an array of numbers:
-const nums = [0, 1, 2, 3, 4];
-let sum = nums.reduce((acc, curr) => {
-  console.log(
-    "Accumulator:", acc,
-    "Current value:", curr,
-    "Total:", acc + curr
-  );
-  return acc + curr;
-}, 10);
-console.log(sum);
+//const nums = [0, 1, 2, 3, 4];
+//let sum = nums.reduce((acc, curr) => {
+  //console.log(
+  //  "Accumulator:", acc,
+    //"Current value:", curr,
+    //"Total:", acc + curr
+  //);
+  //return acc + curr;
+//}, 10);
+//console.log(sum);
 
 
 const teamMembers = [
@@ -42,6 +42,18 @@ const teamMembers = [
 ];
 
 // Totaling a specific object property
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+//console.log(totalExperience);
 
 
 // Grouping by a property, and totaling it too
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+console.log(experienceByProfession);
